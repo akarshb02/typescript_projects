bfebc355f5d68d59988de491ff05cf7805b21592
Akarsh <akarshb02@gmail.com>
2021-04-18 11:49:40 +0530

    passing props

---
+++ hook_props/text.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { App } from "./App";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      oy
+
+      <App text='hello' i={1} />
+    </div>
+  );
+}
+
+export default App;
